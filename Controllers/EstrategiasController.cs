9c86ad49a9d8e44f025546cee38677a19e7814ed
rmachado <rmachado@americastg.com>
2018-07-27 13:36:15 -0300

    first commit

---
+++ Controllers/EstrategiasController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovie.Models;
+using System.Web.Script.Serialization;
+
+
+
+namespace MvcMovie.Controllers
+{
+    public class EstrategiasController : Controller
+    {
+        private EstrategiaDBContext db = new EstrategiaDBContext();
+
+        // GET: Estrategias
+        public ActionResult Index()
+        {
+            
+            return View(db.Estrategias.ToList());
+        }
+
+        // GET: Estrategias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estrategia estrategia = db.Estrategias.Find(id);
+            if (estrategia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estrategia);
+        }
+
+        // GET: Estrategias/Create
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+
+        // POST: Estrategias/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "ID,side,price,quantity,symbol")] Estrategia estrategia)
+        {
+            if (ModelState.IsValid)
+            {
+                 
+                RpcClient connectRMQ = new RpcClient();
+
+                var json = new JavaScriptSerializer().Serialize(estrategia);
+                var jso1 = "{'id': 2,'side':'buy','price':1,'quantity':3,'symbol':'USD'}";
+                var response = connectRMQ.Call(json);
+
+                db.Estrategias.Add(estrategia);
+                db.SaveChanges();
+                var res = response;
+                TempData.Add("MyTempData", res);
+
+                
+                return RedirectToAction("Response1", "Estrategias");
+            }
+
+            return View(estrategia);
+        }
+
+        public ActionResult Response1()
+        {
+            string data = TempData["MyTempData"].ToString();
+            ViewBag.Dat = data;
+            return View("Index", db.Estrategias.ToList());
+            
+        }
+
+        // GET: Estrategias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estrategia estrategia = db.Estrategias.Find(id);
+            if (estrategia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estrategia);
+        }
+
+        // POST: Estrategias/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,side,price,quantity,symbol")] Estrategia estrategia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(estrategia).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(estrategia);
+        }
+
+        // GET: Estrategias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estrategia estrategia = db.Estrategias.Find(id);
+            if (estrategia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estrategia);
+        }
+
+        // POST: Estrategias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Estrategia estrategia = db.Estrategias.Find(id);
+            db.Estrategias.Remove(estrategia);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+      /*  public String sendData() {
+        }*/
+
+
+
+    }
+}
---
+++ Models/Estrategia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovie.Models
+{
+    public class Estrategia
+    {
+        [Required]
+        public int id { get; set; }
+        [Required]
+        public string side { get; set; } // Só pode ser "buy" or "sell" ==> Lembrar de validar no formulario.
+        [Required]
+        public decimal price { get; set; } // Tem que ser positivo
+        [Required]
+        public int quantity { get; set; } // Tem que ser positivo
+        [Required]
+        public string symbol { get; set; }
+
+    }
+
+    public class EstrategiaDBContext : DbContext
+    {
+        public DbSet<Estrategia> Estrategias { get; set; } // Coleção de Estrategias.
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<EstrategiaDBContext>(null);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
